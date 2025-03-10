a5736244fbec25217d9351b2bfec4b1193257871
Peter Mounir Moris <peter.mounir@sehatech.org>
2025-03-10 21:11:49 +0200

    app delegate

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:broker/app/config/style/app_theme.dart';
 import 'package:broker/app/core/binding/initial_binding.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -11,49 +12,47 @@ import 'app/core/heplers/localization_helper.dart';
 import 'app/core/services/storage_service.dart';
 import 'app/routes/app_pages.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+  await Firebase.initializeApp();
 
   await GetStorage.init(); // Initialize GetStorage
-await ScreenUtil.ensureScreenSize();
+  await ScreenUtil.ensureScreenSize();
 
-  final savedLanguage = StorageService.getData<String>('selected_language') ?? 'ar';
+  final savedLanguage =
+      StorageService.getData<String>('selected_language') ?? 'ar';
 
-   LocalizationHelper().changeLocale(
+  LocalizationHelper().changeLocale(
     savedLanguage == 'ar' ? Language.arabic : Language.english,
   );
 
+  runApp(
+    Builder(
+      builder: (context) {
+        ScreenUtil.init(context);
 
+        return ScreenUtilInit(
+          designSize: const Size(375, 812),
+          splitScreenMode: true,
+          minTextAdapt: true,
+          enableScaleText: () => false,
+          enableScaleWH: () => false,
+          builder:
+              (_, child) => OKToast(
+                child: GetMaterialApp(
+                  locale: LocalizationHelper().initialLocale,
+                  translations: LocalizationHelper(),
+                  initialBinding: InitialBinding(),
+                  theme: appTheme,
 
-
-  runApp(Builder(
-    builder: (context) {
-  ScreenUtil.init(context);
-      
-      return ScreenUtilInit(
-        
-        designSize: const Size(375, 812),
-        splitScreenMode: true,
-        minTextAdapt: true,
-        enableScaleText: () => false,
-        enableScaleWH: () => false,
-        builder: (_, child) => OKToast(
-          child: GetMaterialApp(
-      
-          locale: LocalizationHelper().initialLocale,
-            translations: LocalizationHelper(),
-            initialBinding: InitialBinding(),
-            theme: appTheme,
-            
-            debugShowCheckedModeBanner: false,
-            title: "Broker",
-            initialRoute: Routes.splash,
-            getPages: AppPages.routes,
-          ),
-        ),
-      );
-    }
-  ));
+                  debugShowCheckedModeBanner: false,
+                  title: "Broker",
+                  initialRoute: Routes.splash,
+                  getPages: AppPages.routes,
+                ),
+              ),
+        );
+      },
+    ),
+  );
 }
